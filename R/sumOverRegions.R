04d5451582d8146835cb5c58556fc38890811e63
mbstadler <stadler.michael@gmail.com>
2024-07-11 09:45:57 +0200

    add keepZero argument to sumOverRegions

--- R/sumOverRegions.R
+++ R/sumOverRegions.R
@@ -10,12 +10,17 @@
 #'     returned by \code{\link{readBedMethyl}}.
 #' @param regions A \code{\link[GenomicRegions]{GRanges}} object with target
 #'     regions.
+#' @param keepZero Logical scalar. If \code{FALSE} (the default), only elements
+#'     from \code{regions} that contain at least one feature from \code{se} will
+#'     be returned. If \code{TRUE}, all elements of \code{regions} will be
+#'     returned, potentially with zero counts.
 #' @param verbose A logical scalar. If \code{TRUE}, report on progress.
 #' @param ... Additional parameters for \code{\link[scuttle]{aggregateAcrossFeatures}},
 #'     such as \code{BPPARAM} to run the summing in parallel.
 #'
 #' @return A \code{\link[SummarizedExepriment]{RangedSummarizedExperiment}}
-#'     with \code{length(regions)} rows and \code{ncol(se)} columns.
+#'     with up to \code{length(regions)} rows (exactly \code{length(regions)}
+#'     rows if \code{keepZero = TRUE}) and \code{ncol(se)} columns.
 #'     \code{colData(se)}, but not \code{rowData(se)} will be preserved.
 #'
 #' @author Michael Stadler
@@ -40,10 +45,12 @@
 #' @importFrom scuttle aggregateAcrossFeatures
 #'
 #' @export
-sumOverRegions <- function(se, regions, verbose = FALSE, ...) {
+sumOverRegions <- function(se, regions, keepZero = FALSE, verbose = FALSE, ...) {
     # digest arguments
     .assertVector(x = se, type = "RangedSummarizedExperiment")
     .assertVector(x = regions, type = "GRanges")
+    .assertScalar(x = keepZero, type = "logical")
+    .assertScalar(x = verbose, type = "logical")
 
     # find overlaps of features with regions
     if (verbose) {
@@ -66,12 +73,29 @@ sumOverRegions <- function(se, regions, verbose = FALSE, ...) {
                                             ...)
     rownames(tmp) <- NULL
 
+    # keep zeros if requested
+    if (keepZero) {
+        if (verbose) {
+            "filling in zero count regions"
+        }
+        assayList <- lapply(assays(tmp), function(x) {
+            xx <- matrix(data = 0, nrow = length(regions), ncol = ncol(se),
+                         dimnames = list(NULL, colnames(se)))
+            xx[u, ] <- x
+            xx
+        })
+        rrng <- regions
+    } else {
+        assayList <- assays(tmp)
+        rrng <- regions[u]
+    }
+
     # generate new RangedSummarizedExperiment
     if (verbose) {
         message("generating new RangedSummarizedExperiment")
     }
-    seC <- SummarizedExperiment(assays = assays(tmp),
-                                rowRanges = regions[u],
+    seC <- SummarizedExperiment(assays = assayList,
+                                rowRanges = rrng,
                                 colData = colData(se))
     if (!is.null(names(regions))) {
         rownames(seC) <- names(regions)[u]
