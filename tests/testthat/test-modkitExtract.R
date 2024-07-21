4cc5f2c72d0932b1b4ef76cc475e68841585b3d2
mbstadler <stadler.michael@gmail.com>
2024-07-21 19:33:27 +0200

    expand tests for modkitExtract

--- tests/testthat/test-modkitExtract.R
+++ tests/testthat/test-modkitExtract.R
@@ -25,7 +25,9 @@ test_that("modkitExtract works", {
     if (!is.na(v)) {
         # example data
         modbamfile <- system.file("extdata", "6mA_1_10reads.bam", package = "footprintR")
+        modbamfile2 <- system.file("extdata", "5mC_1_10reads.bam", package = "footprintR")
         reg <- as("chr1:6940000-6955000", "GRanges")
+        regs <- as(c("chr1:6940000-6942400", "chr1:6942600-6950000"), "GRanges")
 
         # invalid arguments
         expect_error(modkitExtract(modkit_bin = "error"))
@@ -57,9 +59,10 @@ test_that("modkitExtract works", {
         tmp_tab <- tempfile()
         tmp_calls <- tempfile()
         tmp_log <- tempfile()
-        writeLines("TEST", tmp_log) # write into log file to trigger warning
 
         # remark: some modkit console output cannot be suppressed using sink() or capture.output()
+        # ... one bamfile, one region
+        writeLines("TEST", tmp_log) # write into log file to trigger warning
         suppressMessages({
             expect_message(
                 expect_warning(
@@ -91,8 +94,47 @@ test_that("modkitExtract works", {
         se2 <- getReadDataForRegion(bamfile = modbamfile, region = reg,
                                     arglist.readModkitExtract = list(modbase = "a"))
         expect_identical(se1, se2)
-
-        # clean up
         unlink(c(tmp_tab, tmp_calls, tmp_log))
+
+        # ... one bamfile, no regions
+        res2 <- modkitExtract(modkit_bin = NULL, bamfile = modbamfile,
+                              regions = NULL,
+                              out_extract_table = tmp_tab,
+                              out_read_calls = NULL,
+                              out_log_file = tmp_log,
+                              verbose = FALSE)
+        expect_identical(res2, c(`extract-table` = normalizePath(tmp_tab),
+                                 `read-calls` = NA,
+                                 `run-log` = normalizePath(tmp_log)))
+        lns <- readLines(tmp_tab)
+        expect_length(lns, 33300L)
+        expect_true(grepl("^read_id\tforward_read_position\tref_position\tchrom\tmod_strand\tref_strand", lns[1]))
+        lns <- readLines(tmp_log)
+        expect_identical(lns[1], "TEST")
+        expect_true(grepl("INFO.+processed", lns[length(lns)]))
+        unlink(c(tmp_tab, tmp_log))
+
+        # ... one bamfile, two regions
+        res3 <- modkitExtract(modkit_bin = NULL, bamfile = modbamfile2,
+                              regions = regs,
+                              out_extract_table = NULL,
+                              out_read_calls = tmp_calls,
+                              out_log_file = NULL,
+                              verbose = FALSE)
+        expect_identical(res3, c(`extract-table` = NA,
+                                 `read-calls` = normalizePath(tmp_calls),
+                                 `run-log` = NA))
+        lns <- readLines(tmp_calls)
+        expect_length(lns, 9929L)
+        expect_true(grepl("^read_id\tforward_read_position\tref_position\tchrom\tmod_strand\tref_strand", lns[1]))
+        suppressMessages(
+            expect_message(
+                se3 <- readModkitExtract(tmp_calls, modbase = "a", verbose = TRUE)
+            )
+        )
+        expect_s4_class(se3, "RangedSummarizedExperiment")
+        expect_identical(dim(se3), c(5496L, 4L))
+        expect_true(all(overlapsAny(regs, se3)))
+        unlink(c(tmp_calls))
     }
 })
