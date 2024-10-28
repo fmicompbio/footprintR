10f1fa6b4b48ca008dc7c1118007ce36d881f20a
mbstadler <stadler.michael@gmail.com>
2024-10-28 15:50:36 +0100

    add maxFracLowConf (fail rate) filter

--- R/filterReads.R
+++ R/filterReads.R
@@ -1,30 +1,34 @@
 #' Filter reads
-#' 
+#'
 #' @param se A \code{SummarizedExperiment} object.
 #' @param assay.type.read A character scalar providing the name of a read-level
-#'     assay in \code{se}. This assay will be used to extract read names, as 
-#'     well as to filter out any read that is not overlapping any of the 
+#'     assay in \code{se}. This assay will be used to extract read names, as
+#'     well as to filter out any read that is not overlapping any of the
 #'     positions in the object.
-#' @param readInfoCol A character scalar providing the name of the column in 
-#'     \code{colData} that contains read info. Can be \code{NULL} if no such 
-#'     column exists. 
-#' @param qcCol A character scalar providing the name of the column in 
-#'     \code{colData} that contains quality metrics (calculated by 
-#'     \code{calcReadStats}). Can be \code{NULL} if no such column exists. 
-#' @param minQscore A numeric scalar representing the smallest acceptable 
+#' @param readInfoCol A character scalar providing the name of the column in
+#'     \code{colData} that contains read info. Can be \code{NULL} if no such
+#'     column exists.
+#' @param qcCol A character scalar providing the name of the column in
+#'     \code{colData} that contains quality metrics (calculated by
+#'     \code{calcReadStats}). Can be \code{NULL} if no such column exists.
+#' @param minQscore A numeric scalar representing the smallest acceptable
 #'     read-level Qscore. Reads with Qscore below this value will be filtered
 #'     out.
-#' @param minEntropy A numeric scalar representing the smallest acceptable 
+#' @param minEntropy A numeric scalar representing the smallest acceptable
 #'     read-level entropy. Reads with entropy below this value will be filtered
 #'     out.
-#' @param minReadLength A numeric scalar representing the smallest acceptable 
+#' @param maxFracLowConf A numeric scalar representing the maximally acceptable
+#'     fraction of low-confidence modified base calls in a read. Reads with
+#'     a fraction of low confidence calls greater than this value will be
+#'     filtered out.
+#' @param minReadLength A numeric scalar representing the smallest acceptable
 #'     read length. Reads that are shorter than this value will be filtered
 #'     out.
-#' @param minAlignedLength A numeric scalar representing the smallest acceptable 
-#'     aligned length. Reads with aligned length shorter than this value will 
+#' @param minAlignedLength A numeric scalar representing the smallest acceptable
+#'     aligned length. Reads with aligned length shorter than this value will
 #'     be filtered out.
-#' @param minAlignedFraction A numeric scalar representing the smallest 
-#'     acceptable aligned fraction of a read. Reads where the aligned fraction 
+#' @param minAlignedFraction A numeric scalar representing the smallest
+#'     acceptable aligned fraction of a read. Reads where the aligned fraction
 #'     is smaller than this value will be filtered out.
 #' @param prune A logical scalar. If \code{TRUE} (the default), samples for
 #'     which the filtering retains none of the reads will be completely removed
@@ -32,15 +36,15 @@
 #'     and from assays that do not store read-level data). If \code{FALSE},
 #'     such samples are retained (in the assays with read-level data as a
 #'     zero-column \code{SparseMatrix}).
-#' 
-#' @author Charlotte Soneson
+#'
+#' @author Charlotte Soneson, Michael Stadler
 #' @export
-#' 
+#'
 #' @returns A filtered \code{SummarizedExperiment} object. The metadata of this
 #' object contains a slot named \code{filteredOutReads}, which tabulate all
-#' reads that are filtered out, together with the reason(s) for exclusion. 
-#' 
-#' @examples 
+#' reads that are filtered out, together with the reason(s) for exclusion.
+#'
+#' @examples
 #' library(SummarizedExperiment)
 #' modbamfile <- system.file("extdata", "6mA_1_10reads.bam",
 #'                           package = "footprintR")
@@ -48,19 +52,20 @@
 #'            modbase = "a", verbose = TRUE)
 #' se <- addReadStats(se, name = "QC")
 #' sefilt <- filterReads(se, minQscore = 14, minAlignedLength = 10000)
-#' 
-#' 
+#'
+#'
 #' @importFrom SparseArray SVT_SparseArray rowSums
-#' 
-filterReads <- function(se, assay.type.read = "mod_prob", 
+#'
+filterReads <- function(se, assay.type.read = "mod_prob",
                         readInfoCol = "read_info", qcCol = "QC",
-                        minQscore = 0, minEntropy = 0, 
-                        minReadLength = 0, minAlignedLength = 0, 
-                        minAlignedFraction = 0, prune = TRUE) {
+                        minQscore = 0, minEntropy = 0,
+                        maxFracLowConf = 1, minReadLength = 0,
+                        minAlignedLength = 0, minAlignedFraction = 0,
+                        prune = TRUE) {
     ## Input checks
     .assertVector(x = se, type = "SummarizedExperiment")
     .checkSEValidity(se)
-    .assertScalar(x = assay.type.read, type = "character", 
+    .assertScalar(x = assay.type.read, type = "character",
                   validValues = .getReadLevelAssayNames(se))
     .assertScalar(x = readInfoCol, type = "character", allowNULL = TRUE,
                   validValues = colnames(SummarizedExperiment::colData(se)))
@@ -68,70 +73,74 @@ filterReads <- function(se, assay.type.read = "mod_prob",
                   validValues = colnames(SummarizedExperiment::colData(se)))
     .assertScalar(x = minQscore, type = "numeric")
     .assertScalar(x = minEntropy, type = "numeric")
+    .assertScalar(x = maxFracLowConf, type = "numeric", rngIncl = c(0, 1))
     .assertScalar(x = minReadLength, type = "numeric")
     .assertScalar(x = minAlignedLength, type = "numeric")
-    .assertScalar(x = minAlignedFraction, type = "numeric")
+    .assertScalar(x = minAlignedFraction, type = "numeric", rngIncl = c(0, 1))
     .assertScalar(x = prune, type = "logical")
-    
-    ## Initialize sparse logical array for each sample, which will be TRUE 
+
+    ## Initialize sparse logical array for each sample, which will be TRUE
     ## for reads that are filtered out with respect to the different criteria
     ## Remark: Could move this to a global constant
-    filterNames <- c("Qscore", "Entropy", "ReadLength", "AlignedLength",
-                     "AlignedFraction", "AllNA")
+    filterNames <- c("Qscore", "Entropy", "FracLowConf", "ReadLength",
+                     "AlignedLength", "AlignedFraction", "AllNA")
     readsToRemove <- lapply(
         structure(colnames(se), names = colnames(se)),
         function(nm) {
             SparseArray::SVT_SparseArray(
-                dim = c(ncol(assay(se, assay.type.read)[[nm]]), 
-                        length(filterNames)), 
+                dim = c(ncol(assay(se, assay.type.read)[[nm]]),
+                        length(filterNames)),
                 dimnames = list(colnames(assay(se, assay.type.read)[[nm]]),
                                 filterNames),
                 type = "logical"
         )}
     )
-    
+
     for (nm in colnames(se)) {
         ## Extract read info and QC
-        if (!is.null(readInfoCol) && !is.null(se[[readInfoCol]]) && 
+        if (!is.null(readInfoCol) && !is.null(se[[readInfoCol]]) &&
             (nm %in% names(se[[readInfoCol]]))) {
             ri <- se[[readInfoCol]][[nm]]
         } else {
             ri <- NULL
         }
-        if (!is.null(qcCol) && !is.null(se[[qcCol]]) && 
+        if (!is.null(qcCol) && !is.null(se[[qcCol]]) &&
             (nm %in% names(se[[qcCol]]))) {
             qc <- se[[qcCol]][[nm]]
         } else {
             qc <- NULL
         }
-        
+
         ## Quality score
         if (!is.null(ri) && "qscore" %in% colnames(ri)) {
-            readsToRemove[[nm]][which(ri$qscore < minQscore), 
+            readsToRemove[[nm]][which(ri$qscore < minQscore),
                                 "Qscore"] <- TRUE
         }
-        
+
         ## KS entropy
         if (!is.null(qc) && "SEntrModProb" %in% colnames(qc)) {
             readsToRemove[[nm]][which(qc$SEntrModProb < minEntropy),
                                 "Entropy"] <- TRUE
         }
-        
-        ## Fail rate
-        
-        
+
+        ## Fraction of low-confidence modification calls
+        if (!is.null(qc) && "FracLowConf" %in% colnames(qc)) {
+            readsToRemove[[nm]][which(qc$FracLowConf > maxFracLowConf),
+                                "FracLowConf"] <- TRUE
+        }
+
         ## Read length
         if (!is.null(ri) && "read_length" %in% colnames(ri)) {
             readsToRemove[[nm]][which(ri$read_length < minReadLength),
                                 "ReadLength"] <- TRUE
         }
-        
+
         ## Aligned length
         if (!is.null(ri) && "aligned_length" %in% colnames(ri)) {
             readsToRemove[[nm]][ which(ri$aligned_length < minAlignedLength),
                                  "AlignedLength"] <- TRUE
         }
-        
+
         ## Aligned fraction
         if (!is.null(ri) && "aligned_fraction" %in% colnames(ri)) {
             readsToRemove[[nm]][which(ri$aligned_fraction < minAlignedFraction),
@@ -141,11 +150,11 @@ filterReads <- function(se, assay.type.read = "mod_prob",
         ## NA in all positions
         readsToRemove[[nm]][colnames(
             assay(se, assay.type.read)[[nm]][, SparseArray::colSums(
-                assay(se, assay.type.read)[[nm]], 
-                na.rm = TRUE) == 0]), 
+                assay(se, assay.type.read)[[nm]],
+                na.rm = TRUE) == 0]),
             "AllNA"] <- TRUE
     }
-    
+
     ## Subset
     readsToRemove <- lapply(readsToRemove, function(rr) {
         rr[SparseArray::rowSums(rr, na.rm = TRUE) > 0, ]
@@ -153,9 +162,9 @@ filterReads <- function(se, assay.type.read = "mod_prob",
     sesub <- subsetReads(se = se, reads = lapply(readsToRemove, rownames),
                          prune = prune, invert = TRUE)
     metadata(sesub)$filteredOutReads <- readsToRemove
-    
+
     ## Remove any positions with all NA values
     sesub <- .removeAllNAPositions(sesub, assay.type = assay.type.read)
-    
+
     sesub
 }
--- tests/testthat/test-filterReads.R
+++ tests/testthat/test-filterReads.R
@@ -5,8 +5,8 @@ test_that("filterReads works", {
                      modbase = "a", verbose = FALSE)
     se <- addReadsSummary(se, keep.reads = TRUE)
     se <- addReadStats(se, name = "qcc")
-    
-    expect_error(filterReads(se = "error"), 
+
+    expect_error(filterReads(se = "error"),
                  "'se' must be of class 'SummarizedExperiment'")
     expect_error(filterReads(se = se, assay.type.read = 1),
                  "'assay.type.read' must be of class 'character'")
@@ -26,50 +26,56 @@ test_that("filterReads works", {
                  "'qcCol' must have length 1")
     expect_error(filterReads(se = se, qcCol = "missing"),
                  "'qcCol' must be one of")
-    expect_error(filterReads(se = se, qcCol = "qcc", minQscore = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minQscore = "1"),
                  "'minQscore' must be of class 'numeric'")
-    expect_error(filterReads(se = se, qcCol = "qcc", minQscore = c(1, 2)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minQscore = c(1, 2)),
                  "'minQscore' must have length 1")
-    expect_error(filterReads(se = se, qcCol = "qcc", minEntropy = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minEntropy = "1"),
                  "'minEntropy' must be of class 'numeric'")
-    expect_error(filterReads(se = se, qcCol = "qcc", minEntropy = c(1, 2)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minEntropy = c(1, 2)),
                  "'minEntropy' must have length 1")
-    expect_error(filterReads(se = se, qcCol = "qcc", minReadLength = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", maxFracLowConf = "1"),
+                 "'maxFracLowConf' must be of class 'numeric'")
+    expect_error(filterReads(se = se, qcCol = "qcc", maxFracLowConf = c(0.5, 0.7)),
+                 "'maxFracLowConf' must have length 1")
+    expect_error(filterReads(se = se, qcCol = "qcc", minReadLength = "1"),
                  "'minReadLength' must be of class 'numeric'")
-    expect_error(filterReads(se = se, qcCol = "qcc", minReadLength = c(1, 2)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minReadLength = c(1, 2)),
                  "'minReadLength' must have length 1")
-    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedLength = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedLength = "1"),
                  "'minAlignedLength' must be of class 'numeric'")
-    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedLength = c(1, 2)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedLength = c(1, 2)),
                  "'minAlignedLength' must have length 1")
-    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedFraction = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedFraction = "1"),
                  "'minAlignedFraction' must be of class 'numeric'")
-    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedFraction = c(1, 2)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedFraction = 2),
+                 "'minAlignedFraction' must be within \\[0,1\\]")
+    expect_error(filterReads(se = se, qcCol = "qcc", minAlignedFraction = c(0.5, 1)),
                  "'minAlignedFraction' must have length 1")
-    expect_error(filterReads(se = se, qcCol = "qcc", prune = "1"), 
+    expect_error(filterReads(se = se, qcCol = "qcc", prune = "1"),
                  "'prune' must be of class 'logical'")
-    expect_error(filterReads(se = se, qcCol = "qcc", prune = c(TRUE, FALSE)), 
+    expect_error(filterReads(se = se, qcCol = "qcc", prune = c(TRUE, FALSE)),
                  "'prune' must have length 1")
-    
+
     ## Default arguments - no filtering
     out1 <- filterReads(se, qcCol = "qcc")
     setmp <- se
     metadata(setmp)$filteredOutReads = list(
-        s1 = SVT_SparseArray(dim = c(0, 6),
-                             dimnames = list(character(0), 
-                                             c("Qscore", "Entropy", "ReadLength", 
-                                               "AlignedLength",
+        s1 = SVT_SparseArray(dim = c(0, 7),
+                             dimnames = list(character(0),
+                                             c("Qscore", "Entropy", "FracLowConf",
+                                               "ReadLength", "AlignedLength",
                                                "AlignedFraction", "AllNA"))),
-        s2 = SVT_SparseArray(dim = c(0, 6),
-                             dimnames = list(character(0), 
-                                             c("Qscore", "Entropy", "ReadLength", 
-                                               "AlignedLength",
+        s2 = SVT_SparseArray(dim = c(0, 7),
+                             dimnames = list(character(0),
+                                             c("Qscore", "Entropy", "FracLowConf",
+                                               "ReadLength", "AlignedLength",
                                                "AlignedFraction", "AllNA"))))
     expect_identical(setmp, out1)
-    
+
     ## Some filtering
-    out1 <- filterReads(se, qcCol = "qcc", readInfoCol = "read_info", 
-                        minQscore = 13, minEntropy = 0.2, 
+    out1 <- filterReads(se, qcCol = "qcc", readInfoCol = "read_info",
+                        minQscore = 13, minEntropy = 0.2,
                         minAlignedFraction = 0.8)
     expect_s4_class(out1, "SummarizedExperiment")
     expect_equal(dim(out1)[2], dim(se)[2])
@@ -80,28 +86,28 @@ test_that("filterReads works", {
     expect_equal(rownames(out1$qcc$s2), rownames(se$qcc$s2)[c(4, 6)])
     expect_s4_class(metadata(out1)$filteredOutReads$s1, "SparseMatrix")
     expect_s4_class(metadata(out1)$filteredOutReads$s2, "SparseMatrix")
-    expect_equal(dim(metadata(out1)$filteredOutReads$s1), c(5, 6))
-    expect_equal(dim(metadata(out1)$filteredOutReads$s2), c(8, 6))
-    expect_equal(colnames(metadata(out1)$filteredOutReads$s1), 
-                 c("Qscore", "Entropy", "ReadLength", "AlignedLength",
-                   "AlignedFraction", "AllNA"))
-    
+    expect_equal(dim(metadata(out1)$filteredOutReads$s1), c(5, 7))
+    expect_equal(dim(metadata(out1)$filteredOutReads$s2), c(8, 7))
+    expect_equal(colnames(metadata(out1)$filteredOutReads$s1),
+                 c("Qscore", "Entropy", "FracLowConf", "ReadLength",
+                   "AlignedLength", "AlignedFraction", "AllNA"))
+
     ## Only QC filtering
-    out1 <- filterReads(se, qcCol = "qcc", readInfoCol = NULL, 
-                        minQscore = 13, minEntropy = 0.2, 
-                        minReadLength = 8000, minAlignedLength = 5000, 
+    out1 <- filterReads(se, qcCol = "qcc", readInfoCol = NULL,
+                        minQscore = 13, minEntropy = 0.2, maxFracLowConf = 0.1,
+                        minReadLength = 8000, minAlignedLength = 5000,
                         minAlignedFraction = 0.8)
     expect_s4_class(out1, "SummarizedExperiment")
-    expect_equal(dim(out1), c(8930L, 2L))
-    expect_equal(nrow(out1$qcc$s1), 7L)
-    expect_equal(rownames(out1$qcc$s1), rownames(se$qcc$s1)[-c(2, 3, 9)])
-    expect_equal(nrow(out1$qcc$s2), 6L)
-    expect_equal(rownames(out1$qcc$s2), rownames(se$qcc$s2)[c(1, 2, 4, 6, 8, 9)])
-    
+    expect_equal(dim(out1), c(8549L, 2L))
+    expect_equal(nrow(out1$qcc$s1), 6L)
+    expect_equal(rownames(out1$qcc$s1), rownames(se$qcc$s1)[-c(2, 3, 8, 9)])
+    expect_equal(nrow(out1$qcc$s2), 3L)
+    expect_equal(rownames(out1$qcc$s2), rownames(se$qcc$s2)[c(1, 4, 6)])
+
     ## Only read info filtering
-    out1 <- filterReads(se, qcCol = NULL, readInfoCol = "read_info", 
-                        minQscore = 13, minEntropy = 0.2, 
-                        minReadLength = 8000, minAlignedLength = 5000, 
+    out1 <- filterReads(se, qcCol = NULL, readInfoCol = "read_info",
+                        minQscore = 13, minEntropy = 0.2,
+                        minReadLength = 8000, minAlignedLength = 5000,
                         minAlignedFraction = 0.8)
     expect_s4_class(out1, "SummarizedExperiment")
     expect_equal(dim(out1), c(7691L, 2L))
@@ -111,4 +117,3 @@ test_that("filterReads works", {
     expect_equal(rownames(out1$qcc$s2), rownames(se$qcc$s2)[3:7])
 })
 
-    
