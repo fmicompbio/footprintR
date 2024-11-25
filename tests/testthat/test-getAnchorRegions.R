3ac9bfb1d9d355b7ffd237a973480ea9dbc6d4b8
Charlotte Soneson <charlottesoneson@gmail.com>
2024-11-25 17:50:26 +0100

    Update unit tests for getAnchorRegions

--- tests/testthat/test-getAnchorRegions.R
+++ tests/testthat/test-getAnchorRegions.R
@@ -114,7 +114,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-2, 2))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-2, 2))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 5))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:6929387-6929391:-"), ignore.strand = TRUE)
     se_r2 <- subsetByOverlaps(se, GRanges("chr1:6935628-6935632:-"), ignore.strand = TRUE)
@@ -181,7 +183,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-2, 2))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-2, 2))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 5))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:6929387-6929391:-"), ignore.strand = FALSE)
     se_r2 <- subsetByOverlaps(se, GRanges("chr1:6935628-6935632:-"), ignore.strand = FALSE)
@@ -250,7 +254,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-2, 2))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-2, 2))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 5))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:6929336-6929340:-"), ignore.strand = TRUE)
     expect_true(all(c("chr1:6929338:-", "chr1:6929338:+") %in% rownames(se_r1)))
@@ -346,7 +352,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-4, 4))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-4, 4))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 9))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:6929011-6929019:+"), ignore.strand = FALSE)
     s1_r1 <- which(SparseArray::colSums(assay(se_r1, "mod_prob")$s1 >= 0, na.rm = TRUE) > 0)
@@ -401,7 +409,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-4, 4))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-4, 4))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 9))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:6929011-6929019:+"), ignore.strand = FALSE)
     s1_r1 <- which(SparseArray::colSums(assay(se_r1, "mod_prob")$s1 >= 0, na.rm = TRUE) > 0)
@@ -445,7 +455,9 @@ test_that("getAnchorRegions works", {
     expect_equal(SummarizedExperiment::assayNames(ar1), c("mod_prob", "Nvalid"))
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-4, 4))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-4, 4))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 9))
     # get reads in each sample that overlap each of the regions
     expect_null(assay(ar1, "mod_prob")$s1)
     expect_null(assay(ar1, "mod_prob")$s2)
@@ -472,7 +484,9 @@ test_that("getAnchorRegions works", {
     expect_equal(SummarizedExperiment::assayNames(ar1), c("mod_prob", "Nvalid"))
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-4, 4))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-4, 4))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 9))
     # get reads in each sample that overlap each of the regions
     expect_null(assay(ar1, "mod_prob")$s1)
     expect_null(assay(ar1, "mod_prob")$s2)
@@ -497,7 +511,9 @@ test_that("getAnchorRegions works", {
     expect_s4_class(SummarizedExperiment::assay(ar1, "Nvalid"), "DataFrame")
     expect_s4_class(SummarizedExperiment::assay(ar1, "mod_prob")$s1, "NaArray")
     expect_true(is.matrix(SummarizedExperiment::assay(ar1, "Nvalid")$s1))
-    expect_equal(SummarizedExperiment::rowData(ar1)$relpos, seq(-4, 4))
+    expect_equal(pos(SummarizedExperiment::rowRanges(ar1)), seq(-4, 4))
+    expect_equal(as.character(seqnames(SummarizedExperiment::rowRanges(ar1))),
+                 rep("anchor", 9))
     # get reads in each sample that overlap each of the regions
     se_r1 <- subsetByOverlaps(se, GRanges("chr1:692911-692919:+"), ignore.strand = FALSE)
     s1_r1 <- which(SparseArray::colSums(assay(se_r1, "mod_prob")$s1 >= 0, na.rm = TRUE) > 0)
