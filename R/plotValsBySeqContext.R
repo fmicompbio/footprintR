81400fde10a76ea81b523657e3fb31cd3727da3b
Charlotte Soneson <charlottesoneson@gmail.com>
2026-01-01 12:49:09 +0100

    Expand plotValsBySeqContext to make aggregation and facetting controllable by the user

--- R/plotValsBySeqContext.R
+++ R/plotValsBySeqContext.R
@@ -1,41 +1,55 @@
 #' Plot assay values stratified by sequence context
 #'
 #' @param se \code{\link[SummarizedExperiment]{SummarizedExperiment}} object
-#'     with footprinting data. Rows should correspond to positions
+#'     with read-level footprinting data. Rows should correspond to positions
 #'     and columns to samples.
 #' @param seqContextColumn Character scalar indicating which column of
 #'     \code{rowData(se)} contains the sequence context.
-#' @param assayName Character scalar indicating from which assay the values
-#'     to plot will be extracted. The assay can be a read-level or summary-level
-#'     assay. In both cases, the function will calculate the row means for
-#'     each position across all (reads and) samples in \code{se}.
+#' @param assayName Character scalar indicating from which read-level assay the
+#'     values to plot should be extracted.
+#' @param aggregation Character scalar indicating if/how to aggregate values
+#'     across (samples and) reads before plotting. Must be one of "none" (no
+#'     aggregation, individual values from the assay are plotted) and "mean"
+#'     (calculate average value for each position before plotting).
+#' @param facetBy Character scalar indicating how to facet values in the plot.
+#'     Must be either \code{NULL} (in which case no facetting is done, and
+#'     values are aggregated across all samples in \code{se}) or \code{"sample"}
+#'     (in which case values are aggregated within each sample, and plots are
+#'     facetted accordingly).
 #' @param plotType Character scalar indicating what type of plot to create.
 #'     Should be one of \code{"violin"}, \code{"bar"} and \code{"errorbar"}.
 #' @param topN,bottomN Numeric scalars determining the number of sequence
 #'     contexts to include in the plot. The \code{topN} contexts with the
-#'     highest average values, and the \code{bottomN} contexts with the lowest
-#'     average values will be displayed.
+#'     highest average values and the \code{bottomN} contexts with the lowest
+#'     average values will be displayed. If \code{facetBy = "sample"}, the
+#'     selection of contexts to show is performed separately within each sample.
 #' @param flipCoord Logical scalar indicating whether the plot axes should be
 #'     flipped (to display sequence contexts on the y-axis and values on the
 #'     x-axis) or not.
 #' @param yAxisLabel Character scalar providing the label to display on the
-#'     y-axis (corresponding to the values).
+#'     value axis.
+#'
+#' @returns
+#' A \code{ggplot} object.
 #'
 #' @export
 #' @author Charlotte Soneson
 #'
 #' @examples
-#' modbamfile <- system.file("extdata", "6mA_1_10reads.bam",
+#' modbamfile <- system.file("extdata", c("6mA_1_10reads.bam", "6mA_2_10reads.bam"),
 #'                           package = "footprintR")
 #' ref <- Biostrings::readDNAStringSet(system.file("extdata", "reference.fa.gz",
 #'                                                 package = "footprintR"))
-#' se <- readModBam(bamfiles = modbamfile, regions = "chr1", modbase = "a")
+#' se <- readModBam(bamfiles = modbamfile, regions = "chr1", modbase = "a",
+#'                  BPPARAM = BiocParallel::SerialParam())
 #' se <- addSeqContext(se, sequenceContextWidth = 3, sequenceReference = ref)
 #' se <- filterPositions(se, filters = "sequenceContext", sequenceContext = "NAN")
 #' plotValsBySeqContext(se, assayName = "mod_prob", plotType = "violin",
 #'                      topN = 3, bottomN = 3)
 #' plotValsBySeqContext(se, assayName = "mod_prob", plotType = "bar",
 #'                      topN = 3, bottomN = 3)
+#' plotValsBySeqContext(se, assayName = "mod_prob", plotType = "bar",
+#'                      topN = 3, bottomN = 3, facetBy = "sample")
 #'
 #' @importFrom dplyr group_by mutate ungroup arrange desc select distinct
 #'     bind_rows slice_max slice_min filter summarize
@@ -43,10 +57,15 @@
 #' @importFrom ggplot2 ggplot aes geom_violin theme_bw geom_col geom_linerange
 #'     geom_errorbar coord_flip
 #' @importFrom SparseArray rowSums is_nonna
+#' @importFrom tidytext reorder_within scale_x_reordered
+#' @importFrom stats sd
+#' @importFrom rlang .data
 #'
 plotValsBySeqContext <- function(se,
                                  seqContextColumn = "sequenceContext",
                                  assayName = "mod_prob",
+                                 aggregation = "mean",
+                                 facetBy = NULL,
                                  plotType = "violin",
                                  topN = 10,
                                  bottomN = 10,
@@ -56,8 +75,12 @@ plotValsBySeqContext <- function(se,
     .assertVector(x = se, type = "SummarizedExperiment")
     .assertScalar(x = seqContextColumn, type = "character",
                   validValues = colnames(rowData(se)))
+    .assertScalar(x = aggregation, type = "character",
+                  validValues = c("none", "mean"))
     .assertScalar(x = assayName, type = "character",
-                  validValues = assayNames(se))
+                  validValues = .getReadLevelAssayNames(se))
+    .assertScalar(x = facetBy, type = "character", validValues = "sample",
+                  allowNULL = TRUE)
     .assertScalar(x = plotType, type = "character",
                   validValues = c("violin", "bar", "errorbar"))
     .assertScalar(x = topN, type = "numeric", rngIncl = c(0, Inf))
@@ -65,68 +88,105 @@ plotValsBySeqContext <- function(se,
     .assertScalar(x = flipCoord, type = "logical")
     .assertScalar(x = yAxisLabel, type = "character")
 
-    # calculate row means
-    a <- as.matrix(assay(se, assayName))
-    df <- data.frame(seqContext = rowData(se)[[seqContextColumn]],
-                     vals = rowSums(a, na.rm = TRUE) / rowSums(is_nonna(a)))
+    # calculate plot values
+    if (aggregation == "mean" & is.null(facetBy)) {
+        a <- as.matrix(assay(se, assayName))
+        df <- data.frame(sample = "1",
+                         seqContext = rowData(se)[[seqContextColumn]],
+                         vals = rowSums(a, na.rm = TRUE) / rowSums(is_nonna(a))) |>
+            # remove positions with all NA values (could happen e.g. if the input
+            # se is obtained by subsetting an se with more samples)
+            dplyr::filter(!is.na(.data$vals))
+    } else if (aggregation == "none" & is.null(facetBy)) {
+        a <- as.matrix(assay(se, assayName))
+        nna <- nnawhich(a, arr.ind = TRUE)
+        nnav <- nnavals(a)
+        df <- data.frame(sample = "1",
+                         seqContext = rowData(se)[[seqContextColumn]][nna[, 1]],
+                         vals = nnav)
+    } else if (aggregation == "mean" & facetBy == "sample") {
+        df <- do.call(bind_rows, lapply(colnames(se), function(cn) {
+            a <- assay(se, assayName)[[cn]]
+            data.frame(sample = cn,
+                       seqContext = rowData(se)[[seqContextColumn]],
+                       vals = rowSums(a, na.rm = TRUE) / rowSums(is_nonna(a)))
+        })) |>
+            dplyr::filter(!is.na(.data$vals))
+    } else if (aggregation == "none" & facetBy == "sample") {
+        df <- do.call(bind_rows, lapply(colnames(se), function(cn) {
+            a <- assay(se, assayName)[[cn]]
+            nna <- nnawhich(a, arr.ind = TRUE)
+            nnav <- nnavals(a)
+            data.frame(sample = cn,
+                       seqContext = rowData(se)[[seqContextColumn]][nna[, 1]],
+                       vals = nnav)
+        }))
+    }
+    df$flipCoord <- flipCoord
 
     # calculate mean/sd for each context and select top/bottom ones to include
     dfsum <- df |>
-        group_by(seqContext) |>
-        summarize(valsMean = mean(vals),
-                  valsSd = sd(vals),
+        group_by(.data$seqContext, .data$sample, .data$flipCoord) |>
+        summarize(valsMean = mean(.data$vals),
+                  valsSd = sd(.data$vals),
                   .groups = "drop")
     dfsum <- bind_rows(
-        dfsum |> slice_max(valsMean, n = topN, with_ties = FALSE),
-        dfsum |> slice_min(valsMean, n = bottomN, with_ties = FALSE)
+        dfsum |> group_by(sample) |>
+            slice_max(.data$valsMean, n = topN, with_ties = FALSE),
+        dfsum |> group_by(sample) |>
+            slice_min(.data$valsMean, n = bottomN, with_ties = FALSE)
     ) |>
-        distinct() |>
-        arrange(desc(valsMean))
-
-    # determine order to plot contexts
-    if (flipCoord) {
-        levs <- rev(unique(dfsum$seqContext))
-    } else {
-        levs <- unique(dfsum$seqContext)
-    }
+        ungroup() |>
+        distinct()
 
     # plot
     if (plotType == "violin") {
         df <- df |>
-            dplyr::filter(seqContext %in% dfsum$seqContext) |>
-            mutate(seqContext = factor(seqContext, levels = levs))
-        gg <- ggplot(df, aes(x = seqContext, y = vals))  +
-            geom_violin()
-    } else if (plotType == "bar") {
+            dplyr::filter(.data$seqContext %in% dfsum$seqContext) |>
+            mutate(seqContext = reorder_within(.data$seqContext, by = ifelse(
+                .data$flipCoord, .data$vals, -.data$vals),
+                within = sample, fun = mean))
+        gg <- ggplot(df, aes(x = .data$seqContext, y = .data$vals))  +
+            geom_violin(scale = "width")
+    } else if (plotType %in% c("bar", "errorbar")) {
         dfsum <- dfsum |>
-            mutate(seqContext = factor(seqContext, levels = levs))
+            mutate(seqContext = reorder_within(.data$seqContext, by = ifelse(
+                .data$flipCoord, .data$valsMean, -.data$valsMean),
+                within = sample, fun = mean))
         gg <- ggplot(dfsum,
-               aes(x = seqContext, y = valsMean)) +
+                     aes(x = .data$seqContext, y = .data$valsMean)) +
             geom_col()
-    } else if (plotType == "errorbar") {
-        dfsum <- dfsum |>
-            mutate(seqContext = factor(seqContext, levels = levs))
-        gg <- ggplot(dfsum,
-               aes(x = seqContext, y = valsMean)) +
-            geom_col() +
-            geom_linerange(
-                aes(ymin = valsMean, ymax = valsMean + valsSd)
-            ) +
-            geom_errorbar(
-                aes(ymin = valsMean + valsSd, ymax = valsMean + valsSd),
-                width = 0.2
-            )
+        if (plotType == "errorbar") {
+            gg <- gg +
+                geom_linerange(
+                    aes(ymin = .data$valsMean,
+                        ymax = .data$valsMean + .data$valsSd)
+                ) +
+                geom_errorbar(
+                    aes(ymin = .data$valsMean + .data$valsSd,
+                        ymax = .data$valsMean + .data$valsSd),
+                    width = 0.2
+                )
+        }
     }
-    gg <- gg + theme_bw() +
+    gg <- gg +
+        theme_bw() +
         labs(x = "Sequence context",
              y = paste0(yAxisLabel, ifelse(plotType == "violin", "",
                                            ifelse(plotType == "bar", " (mean)",
                                                   " (mean + sd)"))))
+    if (!is.null(facetBy)) {
+        gg <- gg + facet_wrap(~ sample,
+                              scales = ifelse(flipCoord, "free_y", "free_x"))
+    }
+
     if (flipCoord) {
-        gg <- gg + coord_flip()
+        gg <- gg +
+            coord_flip()
     } else {
         gg <- gg +
             theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
     }
-    gg
+
+    gg + scale_x_reordered()
 }
--- tests/testthat/test-plotValsBySeqContext.R
+++ tests/testthat/test-plotValsBySeqContext.R
@@ -1,10 +1,12 @@
 test_that("plotValsBySeqContext works", {
     # example data
-    modbamfile <- system.file("extdata", "6mA_1_10reads.bam",
-                              package = "footprintR")
+    modbamfiles <- system.file("extdata", c("6mA_1_10reads.bam",
+                                            "6mA_2_10reads.bam"),
+                               package = "footprintR")
     ref <- Biostrings::readDNAStringSet(system.file("extdata", "reference.fa.gz",
                                                     package = "footprintR"))
-    se <- readModBam(bamfiles = modbamfile, regions = "chr1", modbase = "a")
+    se <- readModBam(bamfiles = modbamfiles, regions = "chr1", modbase = "a",
+                     BPPARAM = BiocParallel::SerialParam())
     se <- addSeqContext(se, sequenceContextWidth = 3, sequenceReference = ref)
     se <- filterPositions(se, filters = "sequenceContext", sequenceContext = "NAN")
     se <- flattenReadLevelAssay(se, statistics = "Pmod")
@@ -22,6 +24,18 @@ test_that("plotValsBySeqContext works", {
     expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
                                       assayName = "error"),
                  ".assayName. must be one of")
+    expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
+                                      assayName = "mod_prob", aggregation = 1),
+                 ".aggregation. must be of class .character.")
+    expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
+                                      assayName = "mod_prob", aggregation = "error"),
+                 ".aggregation. must be one of")
+    expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
+                                      assayName = "mod_prob", facetBy = 1),
+                 ".facetBy. must be of class .character.")
+    expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
+                                      assayName = "mod_prob", facetBy = "error"),
+                 ".facetBy. must be one of")
     expect_error(plotValsBySeqContext(se = se, seqContextColumn = "sequenceContext",
                                       assayName = "mod_prob", plotType = 1),
                  ".plotType. must be of class .character.")
@@ -72,23 +86,77 @@ test_that("plotValsBySeqContext works", {
                  ".yAxisLabel. must have length 1")
 
     # works with correct arguments
-    g1 <- plotValsBySeqContext(se = se, assayName = "mod_prob", plotType = "violin",
-                               topN = 2, bottomN = 2, flipCoord = TRUE)
-    g2 <- plotValsBySeqContext(se = se, assayName = "Pmod", plotType = "violin",
-                               topN = 2, bottomN = 2, flipCoord = TRUE)
+    g1 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "violin",
+                               topN = Inf, bottomN = Inf, flipCoord = TRUE,
+                               aggregation = "mean", facetBy = "sample")
     expect_true(ggplot2::is_ggplot(g1))
+    expect_s3_class(g1@data, "data.frame")
+    expect_identical(dim(g1@data), c(7975L + 6949L, 4L))
+
+    g2 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "violin",
+                               topN = Inf, bottomN = Inf, flipCoord = TRUE,
+                               aggregation = "mean", facetBy = NULL)
     expect_true(ggplot2::is_ggplot(g2))
-    expect_identical(g1@data, g2@data)
+    expect_s3_class(g2@data, "data.frame")
+    expect_identical(dim(g2@data), c(8112L, 4L))
 
-    g3 <- plotValsBySeqContext(se = se, assayName = "Pmod", plotType = "bar",
-                               topN = 2, bottomN = 2, flipCoord = TRUE)
-    expect_true(ggplot2::is_ggplot(g3))
-    expect_s3_class(g3@data, "tbl_df")
-    expect_equal(dim(g3@data), c(4L, 3L))
+    # check that subsetting to first sample gives identical values to
+    # facetting within function
+    g3a <- plotValsBySeqContext(se = se[, 1], assayName = "mod_prob",
+                                plotType = "bar", aggregation = "mean",
+                                facetBy = NULL, topN = 2, bottomN = 2,
+                                flipCoord = TRUE)
+    expect_true(ggplot2::is_ggplot(g3a))
+    expect_s3_class(g3a@data, "tbl_df")
+    expect_equal(dim(g3a@data), c(4L, 5L))
+    expect_identical(as.character(g3a@data$seqContext), c("GAG___1", "CAG___1", "TAA___1", "TAT___1"))
+    expect_equal(g3a@data$valsMean, c(0.1656683, 0.151384387, 0.051409203, 0.062818268))
 
-    g4 <- plotValsBySeqContext(se = se, assayName = "Pmod", plotType = "errorbar",
+    g3b <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                                plotType = "bar", aggregation = "mean",
+                                facetBy = "sample", topN = 2, bottomN = 2,
+                                flipCoord = TRUE)
+    expect_true(ggplot2::is_ggplot(g3b))
+    expect_s3_class(g3b@data, "tbl_df")
+    expect_equal(dim(g3b@data), c(8L, 5L))
+    tmp1 <- g3a@data
+    levels(tmp1$seqContext) <- sub("1", "s1", levels(tmp1$seqContext))
+    tmp <- g3b@data |> dplyr::filter(sample == "s1") |>
+        dplyr::left_join(tmp1, by = "seqContext")
+    expect_identical(tmp$valsMean.x, tmp$valsMean.y)
+    expect_identical(tmp$valsSd.x, tmp$valsSd.y)
+    expect_identical(g3b@data |> dplyr::filter(seqContext == "GAG___s1") |> dplyr::pull(valsMean), mean(SummarizedExperiment::assay(se, "Pmod")[SummarizedExperiment::rowData(se)$sequenceContext == "GAG", "s1"], na.rm = TRUE))
+    expect_identical(g3b@data |> dplyr::filter(seqContext == "TAA___s2") |> dplyr::pull(valsSd), sd(SummarizedExperiment::assay(se, "Pmod")[SummarizedExperiment::rowData(se)$sequenceContext == "TAA", "s2"], na.rm = TRUE))
+
+    g4 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "errorbar", aggregation = "mean",
                                topN = 2, bottomN = 2, flipCoord = FALSE)
     expect_true(ggplot2::is_ggplot(g4))
     expect_s3_class(g4@data, "tbl_df")
-    expect_equal(dim(g4@data), c(4L, 3L))
+    expect_equal(dim(g4@data), c(4L, 5L))
+
+    g5 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "errorbar", aggregation = "none",
+                               topN = 2, bottomN = 2, flipCoord = FALSE)
+    expect_true(ggplot2::is_ggplot(g5))
+    expect_s3_class(g5@data, "tbl_df")
+    expect_equal(dim(g5@data), c(4L, 5L))
+
+    g6 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "errorbar", aggregation = "none",
+                               topN = 2, bottomN = 2, flipCoord = FALSE,
+                               facetBy = "sample")
+    expect_true(ggplot2::is_ggplot(g6))
+    expect_s3_class(g6@data, "tbl_df")
+    expect_equal(dim(g6@data), c(8L, 5L))
+
+    g7 <- plotValsBySeqContext(se = se, assayName = "mod_prob",
+                               plotType = "errorbar", aggregation = "mean",
+                               topN = 2, bottomN = 2, flipCoord = FALSE,
+                               facetBy = "sample")
+    expect_true(ggplot2::is_ggplot(g7))
+    expect_s3_class(g7@data, "tbl_df")
+    expect_equal(dim(g7@data), c(8L, 5L))
 })
